f7ed6b1666f9b5ce44ab380fbb798eb20cbc2004
Mehdabol <45933250+Mehdabol@users.noreply.github.com>
2019-12-30 10:49:51 +0330

    change canvas

--- src/app/module/dashboard/components/dashboard-bar-chart/dashboard-bar-chart.component.ts
+++ src/app/module/dashboard/components/dashboard-bar-chart/dashboard-bar-chart.component.ts
@@ -17,75 +17,110 @@ export class DashboardBarChartComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.onSearch();
+    // this.barchart = new Chart('canvas', {
+    //   type: 'bar',
+    //   data: {
+    //     labels: ['فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور', 'مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند'],
+    //     datasets: [
+    //       {
+    //         label: 'My First dataset',
+    //         data: [22, 56, 11, 21],
+    //         backgroundColor: [
+    //           'red',
+    //           'green',
+    //           '#FF00FF',
+    //           '#0000bb'
+    //
+    //
+    //         ],
+    //       },
+    //       {
+    //         label: 'My First dataset',
+    //         data: [33, 221, 5, 21],
+    //         backgroundColor: [
+    //           'red',
+    //           'green',
+    //           '#FF00FF',
+    //           '#0000bb'
+    //
+    //         ],
+    //       }, {
+    //         label: 'My First dataset',
+    //         data: [22, 56, 11, 21],
+    //         backgroundColor: [
+    //           'red',
+    //           'green',
+    //           '#FF00FF',
+    //           '#0000bb'
+    //
+    //         ],
+    //       },
+    //       {
+    //         label: 'My First dataset',
+    //         data: [22, 89, 110, 21],
+    //         backgroundColor: [
+    //           'red',
+    //           'green',
+    //           '#FF00FF',
+    //           '#0000bb'
+    //
+    //         ],
+    //       }
+    //     ]
+    //   },
+    //   options: {
+    //     legend: {
+    //       display: false
+    //     },
+    //     scales: {
+    //       xAxes: [{
+    //         display: true
+    //       }],
+    //       yAxes: [{
+    //         display: true
+    //       }],
+    //     }
+    //   }
+    // });
+  }
+
+  onSearch() {
     this.barchart = new Chart('canvas', {
       type: 'bar',
       data: {
         labels: ['فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور', 'مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند'],
         datasets: [
           {
-            label: 'My First dataset',
-            data: [22, 56, 11, 21],
-            backgroundColor: [
-              'red',
-              'green',
-              '#FF00FF',
-              '#0000bb'
-
-
-
-            ],
+            label: 'کل مناقصات',
+            backgroundColor: '#8e5ea2',
+            data: [11, 24, 27, 33]
           },
           {
-            label: 'My First dataset',
-            data: [33, 221, 5, 21],
-            backgroundColor: [
-              'red',
-              'green',
-              '#FF00FF',
-              '#0000bb'
-
-            ],
+            label: 'مناقصات در دست بررسی',
+            backgroundColor: '#3e95cd',
+            data: [11, 22, 56, 22]
+          },
+          {
+            label: 'مناقصات تایید شده',
+            backgroundColor: '#38a938',
+            data: [45, 70, 34, 24]
           }, {
-            label: 'My First dataset',
-            data: [22, 56, 11, 21],
-            backgroundColor: [
-              'red',
-              'green',
-              '#FF00FF',
-              '#0000bb'
-
-            ],
-          },
-          {
-            label: 'My First dataset',
-            data: [22, 89, 110, 21],
-            backgroundColor: [
-              'red',
-              'green',
-              '#FF00FF',
-              '#0000bb'
-
-            ],
+            label: 'مناقصات رد شده',
+            backgroundColor: '#db6969',
+            data: [23, 54, 32, 78]
           }
         ]
       },
       options: {
-        legend: {
-          display: false
-        },
         scales: {
-          xAxes: [{
-            display: true
-          }],
           yAxes: [{
-            display: true
-          }],
+            ticks: {
+              fontFamily: 'samim'
+            }
+          }]
         }
       }
     });
-  }
-
-  onSearch() {
-
   }
 }
