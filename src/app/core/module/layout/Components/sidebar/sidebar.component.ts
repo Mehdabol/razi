6fd0fa3326ff515d8880ae3b564219c3df7f2d03
Mehdabol <45933250+Mehdabol@users.noreply.github.com>
2020-01-02 09:14:04 +0330

    remove extract icon for sub menu

--- src/app/core/module/layout/Components/sidebar/sidebar.component.ts
+++ src/app/core/module/layout/Components/sidebar/sidebar.component.ts
@@ -10,82 +10,82 @@ export class SidebarComponent implements OnInit, OnDestroy {
   token;
   menu = [];
   menuItemLogin = [
-    {title: 'مناقصات', icon: 'fa fa-home', subMenu: []},
-    {title: 'Issue Tracker', icon: 'fa fa-home', subMenu: []},
-    {title: 'سوالات متداول', icon: 'fa fa-home', subMenu: []},
+    {title: 'مناقصات', icon: 'fa fa-home', subMenu: [] , hasChile: false},
+    {title: 'Issue Tracker', icon: 'fa fa-home', subMenu: [] , hasChile: false},
+    {title: 'سوالات متداول', icon: 'fa fa-home', subMenu: [] , hasChile: false},
   ];
   menuItem = [
     {
-      title: 'اطلاع رسانی', icon: 'fa fa-home', subMenu: [
-        {title: 'مراکز درمانی', path: 'dashboard'},
-        {title: 'نمایندگان', path: 'dashboard'},
-        {title: 'شعب', path: 'dashboard'},
-        {title: 'خدمات بیمه ای', path: 'dashboard'},
+      title: 'اطلاع رسانی', icon: 'fa fa-home', hasChile: true, subMenu: [
+        {title: 'مراکز درمانی', path: 'dashboard' , hasChile: false},
+        {title: 'نمایندگان', path: 'dashboard' , hasChile: false},
+        {title: 'شعب', path: 'dashboard', hasChile: false},
+        {title: 'خدمات بیمه ای', path: 'dashboard' , hasChile: false},
       ]
     },
     {
-      title: 'خدمات بیمه ای', icon: 'fa fa-home', subMenu: [
-        {title: 'بیمه های من', path: 'dashboard'},
-        {title: 'بررسی اصالت بیمه', path: 'dashboard'},
-        {title: 'صدور بیمه نامه های آنلاین', path: 'dashboard'},
-        {title: 'پرداخت اقساط اعلامیه بدهکار', path: 'dashboard'},
-        {title: 'خسارت های درمانی', path: 'dashboard'},
+      title: 'خدمات بیمه ای', icon: 'fa fa-home' , hasChile: true, subMenu: [
+        {title: 'بیمه های من', path: 'dashboard', hasChile: false},
+        {title: 'بررسی اصالت بیمه', path: 'dashboard', hasChile: false},
+        {title: 'صدور بیمه نامه های آنلاین', path: 'dashboard', hasChile: false},
+        {title: 'پرداخت اقساط اعلامیه بدهکار', path: 'dashboard', hasChile: false},
+        {title: 'خسارت های درمانی', path: 'dashboard', hasChile: false},
       ]
     },
     {
-      title: 'فایل ها و برنامه ها', icon: 'fa fa-home', subMenu: [
-        {title: 'دانلود فرمهای پیشنهاد', path: 'dashboard'},
-        {title: 'دانلود نیازمندیهای سیستم جامع', path: 'dashboard'},
-        {title: 'دریافت اپلیکشن همراز', path: 'dashboard'},
-        {title: 'اتوماسیون های اداری', path: 'dashboard'},
+      title: 'فایل ها و برنامه ها', icon: 'fa fa-home', hasChile: true, subMenu: [
+        {title: 'دانلود فرمهای پیشنهاد', path: 'dashboard', hasChile: false},
+        {title: 'دانلود نیازمندیهای سیستم جامع', path: 'dashboard', hasChile: false},
+        {title: 'دریافت اپلیکشن همراز', path: 'dashboard', hasChile: false},
+        {title: 'اتوماسیون های اداری', path: 'dashboard', hasChile: false},
       ]
     },
     {
-      title: 'سوالات متداول برای همه', icon: 'fa fa-home', subMenu: [
+      title: 'سوالات متداول', icon: 'fa fa-home', hasChile: true, subMenu: [
         {
-          title: 'فنی', subMenu: [
-            {title: 'آتش سوزی', path: 'dashboard'},
-            {title: 'اتومبیل', path: 'dashboard'},
-            {title: 'عمر و زندگی', path: 'dashboard'},
-            {title: 'درمان', path: 'dashboard'},
-            {title: 'باربری', path: 'dashboard'},
+          title:  'فنی' , hasChile: true,  subMenu: [
+            {title: 'آتش سوزی', path: 'dashboard' , hasChile: false},
+            {title: 'اتومبیل', path: 'dashboard' , hasChile: false},
+            {title: 'عمر و زندگی', path: 'dashboard' , hasChile: false},
+            {title: 'درمان', path: 'dashboard' , hasChile: false},
+            {title: 'باربری', path: 'dashboard' , hasChile: false},
           ]
         },
-        {title: 'فناوری اطلاعات', path: 'dashboard', subMenu: []},
-        {title: 'برنامه ریزی', path: 'dashboard', subMenu: []},
-        {title: 'مالی', path: 'dashboard', subMenu: []},
-        {title: 'پشتیبانی', path: 'dashboard', subMenu: []},
-        {title: 'بازاریابی', path: 'dashboard', subMenu: []},
-        {title: 'روابط عمومی', path: 'dashboard', subMenu: []},
-        {title: 'حقوقی', path: 'dashboard', subMenu: []},
-        {title: 'بازرسی و کنترل های داخلی', path: 'dashboard', subMenu: []},
-        {title: 'امور نمایندگان', path: 'dashboard', subMenu: []},
-        {title: 'منابع انسانی', path: 'dashboard', subMenu: []},
+        {title: 'فناوری اطلاعات', path: 'dashboard', subMenu: [] , hasChile: false},
+        {title: 'برنامه ریزی', path: 'dashboard', subMenu: [] , hasChile: false},
+        {title: 'مالی', path: 'dashboard', subMenu: [] , hasChile: false},
+        {title: 'پشتیبانی', path: 'dashboard', subMenu: [] , hasChile: false},
+        {title: 'بازاریابی', path: 'dashboard', subMenu: [] , hasChile: false},
+        {title: 'روابط عمومی', path: 'dashboard', subMenu: [] , hasChile: false},
+        {title: 'حقوقی', path: 'dashboard', subMenu: [] , hasChile: false},
+        {title: 'بازرسی و کنترل های داخلی', path: 'dashboard', subMenu: [] , hasChile: false} ,
+        {title: 'امور نمایندگان', path: 'dashboard', subMenu: [] , hasChile: false},
+        {title: 'منابع انسانی', path: 'dashboard', subMenu: [] , hasChile: false},
 
       ]
     },
     {
-      title: 'اطلاعات پایه', icon: 'fa fa-home', subMenu: [
-        {title: 'مدیریت دسترسی', path: 'dashboard'},
-        {title: 'تعریف اشخاص (حقیقی/حقوقی) ', path: 'dashboard'},
+      title: 'اطلاعات پایه', icon: 'fa fa-home' , hasChile: true, subMenu: [
+        {title: 'مدیریت دسترسی', path: 'dashboard' , hasChile: false},
+        {title: 'تعریف اشخاص (حقیقی/حقوقی) ', path: 'dashboard' , hasChile: false},
       ]
     },
     {
-      title: 'ویژه نمایندگان', icon: 'fa fa-home', subMenu: [
-        {title: 'کارمزد نمایندگان', path: 'dashboard'},
-        {title: 'بارگذاری کارمزد نمایندگان', path: 'dashboard'},
-        {title: 'قرارداد نمایندگان', path: 'dashboard'},
+      title: 'ویژه نمایندگان', icon: 'fa fa-home' , hasChile: true, subMenu: [
+        {title: 'کارمزد نمایندگان', path: 'dashboard' , hasChile: false},
+        {title: 'بارگذاری کارمزد نمایندگان', path: 'dashboard' , hasChile: false},
+        {title: 'قرارداد نمایندگان', path: 'dashboard' , hasChile: false},
       ]
     },
     {
-      title: 'ویژه شعب', icon: 'fa fa-home', subMenu: [
-        {title: 'خلاصه عملکرد شعب', path: 'dashboard'},
+      title: 'ویژه شعب', icon: 'fa fa-home' , hasChile: true, subMenu: [
+        {title: 'خلاصه عملکرد شعب', path: 'dashboard' , hasChile: false},
       ]
     },
     {
-      title: 'در خواست ها', icon: 'fa fa-home', subMenu: [
-        {title: 'سئوالات فنی(بیمه ای)', path: 'dashboard'},
-        {title: 'درخواست ملزومات اداری', path: 'dashboard'},
+      title: 'در خواست ها', icon: 'fa fa-home' , hasChile: true, subMenu: [
+        {title: 'سئوالات فنی(بیمه ای)', path: 'dashboard' , hasChile: false},
+        {title: 'درخواست ملزومات اداری', path: 'dashboard' , hasChile: false},
       ]
     },
   ];
