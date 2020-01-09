101f47f5557bd0b4dec260c9cdbfc69eed02be5b
Mehdabol <45933250+Mehdabol@users.noreply.github.com>
2020-01-09 14:01:55 +0330

    add modal

--- src/app/module/Check-authenticity-insurance-policy/components/check-insurance-policy/check-insurance-policy.component.ts
+++ src/app/module/Check-authenticity-insurance-policy/components/check-insurance-policy/check-insurance-policy.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-check-insurance-policy',
@@ -7,12 +8,31 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CheckInsurancePolicyComponent implements OnInit {
   selectInqueryCode = false;
-  selectEstelam = "false";
+  selectEstelam = 'false';
+  closeResult: string;
 
-  constructor() {
+  constructor(private modalService: NgbModal) {
   }
 
   ngOnInit() {
+  }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
   onChange(event) {
